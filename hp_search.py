188d1a7a15f8fa7d56f4a4a274579ca6e04c242c
taivanbat <taevanbat@gmail.com>
2020-06-12 21:18:17 -0600

    removed explanation at beginning of hp_search.py file

--- hp_search.py
+++ hp_search.py
@@ -1,32 +1,6 @@
 """
 Author: Taivanbat "TK" Badamdorj
 github.com/taivanbat
-
-Hyperparameter search wrapper that uses multiple GPUs.
-
-Advantages:
-    - Automates hyperparameter search
-    - Automatically picks free GPUs
-    - Works for any train.py file that allows hyperparameter specification using
-    command line arguments (see demo)
-    - Both grid search and random search available
-
-Assumes:
-    - train.py can have GPU specified using --gpu_num `GPU_ID` file
-    - train.py keeps track of metrics in separate files e.g. Tensorboard logs
-
-Please note that models are pre-assigned to GPUs and each model is run sequentially
-on a given GPU.
-
-Example:
-    If there are 9 models to try out, and we have three GPUs,
-    each GPU is assigned three models.
-
-    All three GPUs will start training the first models they were assigned.
-    Once a GPU finishes training a model, it will start training the
-    next model it was assigned.
-
-Useful when models are of similar size and run for the same number of epochs.
 """
 
 import argparse
@@ -392,7 +366,7 @@ class HPSearch:
                 id of GPU to use
         """
         for i, hparam in enumerate(hparams):
-            pprint(f'Running process {i} on gpu {gpu}')
+            print(f'Running process {i} on gpu {gpu}')
             pprint(hparam)
             p = self._run_trainer(hparam, gpu)
             p.wait()
